203bfd9f50574d207cef4ae7f3d11c1ac88c4689
Ercüment Türk <ercument.turk@kentkart.com.tr>
2019-05-23 14:21:28 +0300

    Initial version

---
+++ include/config.h
@@ -0,0 +1,11 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/embedian/smarcfimx6/ddr/mx6q_4x_k4b2g1646q.cfg
+#define CONFIG_MX6Q	1
+#define CONFIG_SER3	1
+#define CONFIG_SYS_USE_SPINOR	1
+#define CONFIG_BOARDDIR board/embedian/smarcfimx6
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/smarcfimx6.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
